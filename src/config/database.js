69c57921d0be3e8662e3e68b01a296045cab28c2
Niraj Sahu <nirajkumar46@gmail.com>
2025-03-20 11:05:34 +0530

    add env configurable

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  await mongoose.connect(
-    "mongodb+srv://namastedev:AYSjTw5Z9HEpqhPU@namastenode.erlnp.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.DB_CONNECTION_STRING);
 };
 
 module.exports = connectDb;
